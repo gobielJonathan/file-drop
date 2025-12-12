77fb553673ace2633df125c1fe02ab640079c548
gobiel <gobielj@DNID4099L01.local>
2025-12-12 10:23:46 +0700

    chore: change placeholde sender id at reciever

--- src/Receiver.vue
+++ src/Receiver.vue
@@ -205,7 +205,7 @@ const onQrCodeDetect = (detectedCodes) => {
 
                 <div class="flex items-center">
                     <input v-model="senderUniqueId" type="text" name="" id=""
-                        placeholder="Enter Sender ID (e.g. ABC-123-XYZ)"
+                        placeholder="Enter Sender ID (e.g. abcde)"
                         class="flex h-9 w-full rounded-md border border-input bg-background px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm font-mono text-sm">
                     <button class="p-2 cursor-pointer" @click="isOpenModalScanQr = true">
                         <MaterialSymbolsAndroidCamera />
